7cf2dfe94608959ca7c8b052a3f74d7d2f452206
Todd Rizzolo <todd.rizzolo@lifeway.com>
2018-05-17 12:48:20 -0500

    Update some metadata

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-logo" alt="logo">⛳️  🏌🏼</h1>
+          <h1 className="App-title">Golf League</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Game on.
         </p>
       </div>
     );
